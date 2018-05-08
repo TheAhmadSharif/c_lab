6f90abc82ac76d67c309f343636d37d69ae9e6ec
Ahmad Sharif <ahmadshorif@gmail.com>
2018-05-08 20:49:51 +0600

    problem-11 | problem-38

--- 1-52_Problem/problem-11.c
+++ 1-52_Problem/problem-11.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main () {
-	int i, factorial;
-	printf("Insert a numer to get factorial\n");
+	int i, f, range;
+	long factorial;
+	printf("Please insert a number to get its factorial value\n");
+	scanf("%d", &range);
+	f = factorial = range;
 
-	scanf("%d", &factorial);
-	for(i=factorial-1; i >= 1; i = i - 1) {
+	for(i=1; i < f; i++) {
 		factorial = factorial * i;
-	}	
-	
-	printf("%d\n", factorial);
-
+	}
+	printf("%li\n", factorial);
 	return 0;
 }
